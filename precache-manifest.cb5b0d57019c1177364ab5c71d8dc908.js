8bdfa77c4692cdfdd7266cf18855aeaa2125f3c8
Karolina Kazmierska <karo.a.kazmierska@gmail.com>
2018-11-01 23:04:19 +0100

    Updates

---
+++ precache-manifest.cb5b0d57019c1177364ab5c71d8dc908.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = [
+  {
+    "revision": "e508346c9e61367df056",
+    "url": "/spot-the-ball/static/css/main.965fd816.chunk.css"
+  },
+  {
+    "revision": "e508346c9e61367df056",
+    "url": "/spot-the-ball/static/js/main.e508346c.chunk.js"
+  },
+  {
+    "revision": "cac0487626b94cb3c10f",
+    "url": "/spot-the-ball/static/js/1.cac04876.chunk.js"
+  },
+  {
+    "revision": "149f18c55e26a11301c6",
+    "url": "/spot-the-ball/static/js/runtime~main.149f18c5.js"
+  },
+  {
+    "revision": "83473977cfd9b99554bb305cb6a47889",
+    "url": "/spot-the-ball/static/media/field.83473977.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spot-the-ball/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "34d99de251df6d0636b09bcbeea17fdd",
+    "url": "/spot-the-ball/static/media/A1.34d99de2.jpg"
+  },
+  {
+    "revision": "dd59dfbf9febeb96c448110910c8328f",
+    "url": "/spot-the-ball/static/media/A2.dd59dfbf.jpg"
+  },
+  {
+    "revision": "ff12542e5f57bfe52c72d05c90c24bea",
+    "url": "/spot-the-ball/static/media/B1.ff12542e.jpg"
+  },
+  {
+    "revision": "62d031c66d5562efa99820ef5a707d31",
+    "url": "/spot-the-ball/static/media/B2.62d031c6.jpg"
+  },
+  {
+    "revision": "f445a93c176719602b3cceef4117bf7d",
+    "url": "/spot-the-ball/static/media/C1.f445a93c.jpg"
+  },
+  {
+    "revision": "b1946dabf41c969031e75569878c84ff",
+    "url": "/spot-the-ball/static/media/C2.b1946dab.jpg"
+  },
+  {
+    "revision": "ff611108816ec3fe2454a70ddfa6c2f4",
+    "url": "/spot-the-ball/static/media/D1.ff611108.jpg"
+  },
+  {
+    "revision": "e252e57a17e4b546e1d31a392bec4a87",
+    "url": "/spot-the-ball/static/media/D2.e252e57a.jpg"
+  },
+  {
+    "revision": "4044ac977900b48669bc80f7a57f5835",
+    "url": "/spot-the-ball/static/media/E1.4044ac97.jpg"
+  },
+  {
+    "revision": "e2e6f9634782891fbf99e38634a03ba2",
+    "url": "/spot-the-ball/static/media/E2.e2e6f963.jpg"
+  },
+  {
+    "revision": "23ebc12a5c3a0e3b9d0efb7f96d03589",
+    "url": "/spot-the-ball/static/media/F1.23ebc12a.jpg"
+  },
+  {
+    "revision": "5f9caed06c6eba6e83906a05ab42fd37",
+    "url": "/spot-the-ball/static/media/F2.5f9caed0.jpg"
+  },
+  {
+    "revision": "e020131dbcc2b447c50ef3264c7480ad",
+    "url": "/spot-the-ball/static/media/G1.e020131d.jpg"
+  },
+  {
+    "revision": "3afa52a6b0b91659831d9e113c110822",
+    "url": "/spot-the-ball/static/media/G2.3afa52a6.jpg"
+  },
+  {
+    "revision": "22972b9bcf6bdbfebb7fb711170111ec",
+    "url": "/spot-the-ball/static/media/H1.22972b9b.jpg"
+  },
+  {
+    "revision": "74a22a00dcd8d286cb030b8dcc255765",
+    "url": "/spot-the-ball/static/media/H2.74a22a00.jpg"
+  },
+  {
+    "revision": "4bc73dff3975eb5632a4fac3a5d97780",
+    "url": "/spot-the-ball/static/media/I1.4bc73dff.jpg"
+  },
+  {
+    "revision": "05bd8b08aaaf559dfc0821128e26ea88",
+    "url": "/spot-the-ball/static/media/I2.05bd8b08.jpg"
+  },
+  {
+    "revision": "85d0a603f0d2fe2ca4adac78ee35b423",
+    "url": "/spot-the-ball/static/media/J1.85d0a603.jpg"
+  },
+  {
+    "revision": "fb1b8fb2deda5a20a3ce39adec927aa3",
+    "url": "/spot-the-ball/static/media/J2.fb1b8fb2.jpg"
+  },
+  {
+    "revision": "72c92bb2999eba4424a4bb8b736f266c",
+    "url": "/spot-the-ball/static/media/K1.72c92bb2.jpg"
+  },
+  {
+    "revision": "79760262e0b971bb1729516df16ff20a",
+    "url": "/spot-the-ball/static/media/K2.79760262.jpg"
+  },
+  {
+    "revision": "a4353e00dfaad17b059bb9364219b363",
+    "url": "/spot-the-ball/static/media/L1.a4353e00.jpg"
+  },
+  {
+    "revision": "41990f690d663a6dd6eab475d9c53dd9",
+    "url": "/spot-the-ball/static/media/L2.41990f69.jpg"
+  },
+  {
+    "revision": "324b050b8443e307d462a2a8ef288366",
+    "url": "/spot-the-ball/static/media/M1.324b050b.jpg"
+  },
+  {
+    "revision": "438252de781b226e0c51418c433aa5b1",
+    "url": "/spot-the-ball/static/media/M2.438252de.jpg"
+  },
+  {
+    "revision": "be3b9bd083b605bfee10b42ae06880b9",
+    "url": "/spot-the-ball/static/media/N1.be3b9bd0.jpg"
+  },
+  {
+    "revision": "f01b52f9c0310e3e390a6fbb17448cfc",
+    "url": "/spot-the-ball/static/media/N2.f01b52f9.jpg"
+  },
+  {
+    "revision": "dce65064e72c53014c81791fa0c60aad",
+    "url": "/spot-the-ball/static/media/O1.dce65064.jpg"
+  },
+  {
+    "revision": "2fb5ab24747bce6de6a69bd1efeaf519",
+    "url": "/spot-the-ball/static/media/O2.2fb5ab24.jpg"
+  },
+  {
+    "revision": "942c3ffca35f9e5fe78fc60aebf2cb0e",
+    "url": "/spot-the-ball/static/media/P1.942c3ffc.jpg"
+  },
+  {
+    "revision": "3fec95965c762f036ae902ac08de64f4",
+    "url": "/spot-the-ball/static/media/P2.3fec9596.jpg"
+  },
+  {
+    "revision": "ca81a64293a8b3b0efe46cb1c2508ca3",
+    "url": "/spot-the-ball/static/media/R1.ca81a642.jpg"
+  },
+  {
+    "revision": "d9e72564ae73b6acdb1d03e0ac42f05a",
+    "url": "/spot-the-ball/static/media/R2.d9e72564.jpg"
+  },
+  {
+    "revision": "9e54a44778cb27f546e0bb1db3c0f6e5",
+    "url": "/spot-the-ball/static/media/S1.9e54a447.jpg"
+  },
+  {
+    "revision": "ce904b18e1e6f07c68d728f42a134af0",
+    "url": "/spot-the-ball/static/media/S2.ce904b18.jpg"
+  },
+  {
+    "revision": "eeea47c36b21ec38f869656e1397f4b3",
+    "url": "/spot-the-ball/static/media/T1.eeea47c3.jpg"
+  },
+  {
+    "revision": "895990c7f4e077ffa5d5e65ae07c0cb7",
+    "url": "/spot-the-ball/static/media/T2.895990c7.jpg"
+  },
+  {
+    "revision": "0d734e7afe6b6853a05e6c94007bf998",
+    "url": "/spot-the-ball/index.html"
+  }
+];
